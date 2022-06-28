b605750ce0dfe61b26fb5f864aba49040d786ac9
Valentin Leclerc <leclerc.valentin94@gmail.com>
2022-06-28 11:54:42 +0200

    numbers can be randomized

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+function useMachine() {
+    const goButton = document.getElementById("go");
+    goButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        updateRow();
+    });
+}
+
 function updateRow() {
     for (let i=1; i<=3; i++) {
         let row = document.getElementById(`row${i}`);
         for (let j=1; j<=3; j++) {
+            let cellToRemove = document.getElementById(`cell${j}row${i}`);
+            row.removeChild(cellToRemove);
             let cell = document.createElement("td");
-            cell.innerHTML += newNumber();
+            cell.id = `cell${j}row${i}`;
+            cell.innerHTML = newNumber();
             row.appendChild(cell);
         }
     }
 }
 
 function newNumber() {
-    return toString(Math.floor(Math.random()*10));
+    return Math.floor(Math.random()*10);
 };
+
+useMachine();
